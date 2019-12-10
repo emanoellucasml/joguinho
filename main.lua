54d9ce2caf580d673e12e751f427223fd1c00cdf
Emanoel Lucas <emanoellucas123456@gmail.com>
2019-12-10 12:31:40 -0300

    finalização parcial

--- main.lua
+++ main.lua
@@ -1,9 +1,10 @@
 local r = nil
 local x = nil
 local y = nil
-local score = 0
+local score = nil
 local bola1 = {}
 local background = nil
+local playOnce = true
 bola2 = {}
 bola3 = {}
 bola4 = {}
@@ -13,19 +14,64 @@ bola7 = {}
 bola8 = {}
 bola9 = {}
 bola10 = {}
+bolaComida = {}
 comedor = {}
+
 local menuInicialAtivo = true
-local perdeu = false
+local perdeu = nil
+
+-- funções relativas ao objeto comedor
+function getComedorX(comedor)
+    return comedor.posX
+end
+
+function getComedorY(comedor)
+    return comedor.posY
+end
+---
 
 
+function perdeu()
+    if(perdeu == true) then
+        return true
+    end
+    return false
+end
 
+function reproduzSomDaDerrota()
+    if(playOnce) then
+        somDerrota:play()
+        playOnce = false
+    end
+end
 
-function atualizaBola(bola)
+function atualizaBola(bola, comedor)
     if(bola.y >= 599) then
         bola.y = 0
         bola.x = math.random(1, 790)
     else
         bola.y = bola.y + bola.f
+    end
+
+    if(bola.y >= 575 and (bola.x >= comedor.posX and bola.x <= comedor.posX + 50)) then
+        perdeu = true
+    end
+end
+
+
+function atualizaBolaComida(bolaComida, comedor)
+    if(bolaComida.y >= 599) then
+        bolaComida.y = 0
+        bolaComida.x = math.random(1, 790)
+    else
+        bolaComida.y = bolaComida.y + bolaComida.f
+    end
+
+    if(bolaComida.y >= 575 and (bolaComida.x >= comedor.posX and bolaComida.x <= comedor.posX + 50)) then
+        bolaComida.y = 0
+        bolaComida.f = math.random(5, 15)
+        score = score + 1 -- atualiza o score caso o comedor coma a bola
+        somComeu:play()
     end
 end
 
@@ -44,24 +90,19 @@ function movimentoDireita()
     else
         comedor.posX = comedor.posX + 7.5
     end
-end
-
-function perdeu()
-    if(score == 400 or score > 400) then
-        return true
-    end
-    return false
-end
+end 
 
 function love.load()
     math.randomseed(os.time())
-
+    perdeu = false
+    score = 0
     --CARREGANDO IMAGENS
     background = love.graphics.newImage("assets/img/green-back.png")
 
     --SONS USADOS DENTRO DO JOGO
     musicaTema = love.audio.newSource("assets/songs/song.mp3","static")
     somDerrota = love.audio.newSource("assets/songs/losssong.mp3", "static")
+    somComeu = love.audio.newSource("assets/songs/comeu.wav", "static")
 
 
     --REPRODUZINDO MÚSICA PRINCIPAL
@@ -69,7 +110,8 @@ function love.load()
 	--musicaTema:setLooping(true)
 
     --INICIALIZA AS BOLAS
-    bola1 = {raio = 10, x = math.random(1, 790), y = 0, f = math.random(2, 5)}
+    bolaComida = {raio = 10, x = math.random(1, 790), y = 0, f = math.random(2, 12)}
+    bola1 = {raio = 10, x = math.random(1, 790), y = 0, f = math.random(4, 9)}
     bola2 = {raio = 10, x = math.random(1, 790), y = 0, f = math.random(2, 7)}
     bola3 = {raio = 10, x = math.random(1, 790), y = 0, f = math.random(4, 9)}
     bola4 = {raio = 10, x = math.random(1, 790), y = 0, f = math.random(3, 6)}
@@ -89,54 +131,52 @@ end
 
 
 function love.update(dt)
-    atualizaBola(bola1)
-    atualizaBola(bola2)
-    atualizaBola(bola3)
-    atualizaBola(bola4)
-    atualizaBola(bola5)
-    atualizaBola(bola6)
-    atualizaBola(bola7)
-    atualizaBola(bola8)
-    atualizaBola(bola9)
-    atualizaBola(bola10)
+    atualizaBola(bola1, comedor)
+    atualizaBola(bola2, comedor)
+    atualizaBola(bola3, comedor)
+    atualizaBola(bola4, comedor)
+    atualizaBola(bola5, comedor)
+    atualizaBola(bola6, comedor)
+    atualizaBola(bola7, comedor)
+    atualizaBola(bola8, comedor)
+    atualizaBola(bola9, comedor)
+    atualizaBola(bola10, comedor)
+    atualizaBolaComida(bolaComida, comedor)
     if(love.keyboard.isDown("left")) then
         movimentoEsquerda()
     end
-
     if(love.keyboard.isDown("right")) then 
         movimentoDireita()
     end
-
-    score = score + 1 --atualiza a pontuação
-
-    
-    
 end
 
 function love.draw()
     love.graphics.draw(background, 0, 0)
     if(menuInicialAtivo == true) then
-        love.graphics.print("PRESSIONE ESPAÇO PARA INICIAR", love.graphics.getHeight()/2, love.graphics.getWidth()/2)
+        love.graphics.print("PRESSIONE ESPAÇO PARA INICIAR", love.graphics.getWidth()/2 - 100, love.graphics.getHeight()/2)
         if(love.keyboard.isDown("space")) then 
             menuInicialAtivo = false
         end
-    elseif(perdeu() == true) then
+    elseif(perdeu == true) then
         musicaTema:stop()
-        somDerrota:play()
-        love.graphics.print("VOCÊ PERDEU! PRESSIONE ESPAÇO PARA INICIAR NOVAMENTE", love.graphics.getHeight()/2 - 80, love.graphics.getWidth()/2 - 140)
+        love.graphics.print("VOCÊ PERDEU! PRESSIONE ESPAÇO PARA INICIAR NOVAMENTE", love.graphics.getHeight()/2 - 60, love.graphics.getWidth()/2 - 140)
+        reproduzSomDaDerrota()
         if(love.keyboard.isDown("space")) then 
-            score = 0 --score maior ou igual a 400 é a condição para que o jogador perca. Portanto, se o score zerar, o jogo reiniciará.
+            score = 0 --
+            playOnce = true
+            perdeu = false
         end
     else 
         musicaTema:play()
-	    musicaTema:setLooping(true)
+        musicaTema:setLooping(true)
+        love.graphics.setColor(255,255,0)
+        love.graphics.circle("fill", bolaComida.x, bolaComida.y, bolaComida.raio)
+        love.graphics.setColor(255, 255, 255)
         love.graphics.circle("fill", bola1.x, bola1.y, bola1.raio)
         love.graphics.circle("fill", bola2.x, bola2.y, bola2.raio)
         love.graphics.circle("fill", bola3.x, bola3.y, bola3.raio)
-        love.graphics.circle("fill", bola4.x, bola4.y, bola4.raio)
-        love.graphics.setColor(255,255,0)
+        love.graphics.circle("fill", bola4.x, bola4.y, bola4.raio)        
         love.graphics.circle("fill", bola5.x, bola5.y, bola5.raio) --pintando uma bola de amarelho, pois essa é a comida
-        love.graphics.setColor(255, 255, 255)
         love.graphics.circle("fill", bola6.x, bola6.y, bola6.raio)
         love.graphics.circle("fill", bola7.x, bola7.y, bola7.raio)
         love.graphics.circle("fill", bola8.x, bola8.y, bola8.raio)
